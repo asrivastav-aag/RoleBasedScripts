77d9ae9e4d8356ad820175fa65600b2faa3a6d10
Avinash Srivastava (Sri) <avinash.srivastava@alaskaair.com>
2022-12-06 23:16:08 -0800

    Added DisplayMACaddr function in filters/mac_filter.py to display all MAC Address per VLAN basis

--- venv_ansible/projects/plugins/filters/mac_filter.py
+++ venv_ansible/projects/plugins/filters/mac_filter.py
@@ -3,7 +3,8 @@ class FilterModule:
 
     def filters(BasicInfo):
         return {
-            'TotalMACaddr' : FilterModule.TotalMACaddr
+            'TotalMACaddr' : FilterModule.TotalMACaddr,
+            'DisplayMACaddr' : FilterModule.DisplayMACaddr
             }
     
     def TotalMACaddr (mac_table):
@@ -14,3 +15,16 @@ class FilterModule:
                 for key in key_vars[keys]['mac_addresses']:
                     mac_list.append(key)
         return len(mac_list)
+    
+    def DisplayMACaddr (mac_table):
+        del mac_table['mac_table']['vlans']['all']
+        mac_per_vlan = mac_table['mac_table']['vlans']
+        mac_list_dict = {}
+        for vlan_keys,vlan_values in mac_per_vlan.items():
+            # print(vlan_keys)
+            # print(vlan_values['mac_addresses'].keys())
+            mac_list_dict.update({
+                "VLAN {}".format(vlan_keys) : list(vlan_values['mac_addresses'].keys())
+            })
+        
+        return mac_list_dict
