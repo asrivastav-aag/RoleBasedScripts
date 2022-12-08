10de52bc73dc710a1633e26134f8619553921612
Avinash Srivastava (Sri) <avinash.srivastava@alaskaair.com>
2022-12-07 20:55:19 -0800

    Added 'DisplayConnectedMAC' function to filters/mac_filter.py to display only connected MAC Addresses with Interface info; Updated displayMessages.yaml with new filter

--- venv_ansible/projects/plugins/filters/mac_filter.py
+++ venv_ansible/projects/plugins/filters/mac_filter.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 class FilterModule:
 
-    def filters(BasicInfo):
+    def filters(TotalMACaddr):
         return {
             'TotalMACaddr' : FilterModule.TotalMACaddr,
-            'DisplayMACaddr' : FilterModule.DisplayMACaddr
+            'DisplayMACaddr' : FilterModule.DisplayMACaddr,
+            'DisplayConnectedMAC' : FilterModule.DisplayConnectedMAC
             }
-    
+
+
     def TotalMACaddr (mac_table):
         key_vars = mac_table['mac_table']['vlans']
         mac_list = []
@@ -15,7 +17,8 @@ class FilterModule:
                 for key in key_vars[keys]['mac_addresses']:
                     mac_list.append(key)
         return len(mac_list)
-    
+
+
     def DisplayMACaddr (mac_table):
         del mac_table['mac_table']['vlans']['all']
         mac_per_vlan = mac_table['mac_table']['vlans']
@@ -28,3 +31,18 @@ class FilterModule:
             })
         
         return mac_list_dict
+
+
+    def DisplayConnectedMAC (mac_table):
+        del mac_table['mac_table']['vlans']['all']
+        mac_per_vlan = mac_table['mac_table']['vlans']
+        mac_inf_dict = {}
+        for vlan_values in mac_per_vlan.values():
+            for mac_values in vlan_values['mac_addresses'].values():
+                for inf_keys in mac_values['interfaces']:
+                    if mac_values['interfaces'][inf_keys]['entry_type'] == 'static':
+                        mac_inf_dict.update({
+                            mac_values['mac_address'] : mac_values['interfaces'][inf_keys]['interface']
+                        })
+        
+        return mac_inf_dict
